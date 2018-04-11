4a7642b777d8824b668db13cab827e2c89daaad4
Austin <davisaustin@berkeley.edu>
2018-04-10 17:58:41 -0700

    importing changes

---
+++ 3D_Music_Visualizer/Assets/Scripts/Cube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+    public int band;
+    public float startScale;
+    public float scaleMultiplier;
+    // Use this for initialization
+    void Start () {
+		for (int i = 0; i < 8; i++)
+        {
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < 8; i++)
+        {
+            
+            transform.localScale = new Vector3(1.0f, (startScale + SpectrumAnalyzer.audioBands[band - 1] * scaleMultiplier), 1.0f);
+
+
+            
+        }
+    }
+}
---
+++ 3D_Music_Visualizer/Assets/Scripts/SpectrumAnalyzer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectrumAnalyzer : MonoBehaviour {
+
+    float[] freqBandHighest = new float[8];
+    public static float[] audioBands = new float[8];
+
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (freqBand[i] > freqBandHighest[i])
+            {
+                freqBandHighest[i] = freqBand[i];
+            }
+            audioBands[i] = freqBand[i] / freqBandHighest[i];
+        }
+    }
+
+    public static float[] freqBand = new float[8];
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+
+        // Iterate through the 8 bins.
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i + 1);
+
+            // Adding the remaining two samples into the last bin.
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            // Go through the number of samples for each bin, add the data to the average
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count];
+                count++;
+            }
+
+            // Divide to create the average, and scale it appropriately.
+            average /= count;
+            freqBand[i] = (i + 1) * 100 * average;
+        }
+    }
+    AudioSource audioSource;
+    public static float[] samples = new float[512];
+
+	// Use this for initialization
+
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+	// Update is called once per frame
+	void Update () {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        CreateAudioBands();
+	}
+}
