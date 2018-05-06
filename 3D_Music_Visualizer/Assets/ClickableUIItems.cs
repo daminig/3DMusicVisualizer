a0f12109f91e5bd8b196c534a2236654aa701661
ANTHONY GARCIA <antg02@berkeley.edu>
2018-05-05 20:47:33 -0700

    Created script to make all FileBrowser UI elements VR clickable

---
+++ 3D_Music_Visualizer/Assets/ClickableUIItems.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* Makes Button GameObjects clickable in VR by checking if GameObject has "Button" script. 
+ * Checks every child of the UI.
+*/
+
+public class ClickableUIItems : MonoBehaviour {
+
+	Transform uielement;
+
+	// Use this for initialization
+	void Start () {
+		uielement = this.gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Component[] test = uielement.gameObject.GetComponentsInChildren (typeof(Button), false);
+
+		for (int i = 0; i < test.Length; i++) {
+			if (test[i].gameObject.GetComponent<BoxCollider> () == null) {  //To check if already has BoxCollider
+				MakeClickable (test[i].gameObject);
+			}
+		}
+	}
+
+	bool IsButton(GameObject o) {
+		if (o.GetComponent<Button>() != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void MakeClickable(GameObject o) {
+		if (IsButton (o)) {
+			o.AddComponent<BoxCollider> ();
+			o.AddComponent<VRUIItem> ();
+		}
+
+	}
+
+
+}
